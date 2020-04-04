eca3ac3205c9d311ea6d1c07162a281e5a6e5b9e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-04 12:42:57 +0100

    ft: add basic test and build logic

---
+++ ReadTimeEstimator.Tests/Class1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace ReadTimeEstimator.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            (true).Should().BeTrue();
+        }
+    }
+}
