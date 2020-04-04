85bddefc72e327fe7d6ce2010a2c2f7ac567f1ae
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-04 17:38:18 +0100

    ft: update the humanizer tests

--- ReadTimeEstimator.Tests/UtilitiesTests.cs
+++ ReadTimeEstimator.Tests/UtilitiesTests.cs
@@ -6,11 +6,27 @@ namespace ReadTimeEstimator.Tests
     public class UtilitiesTests
     {
         [Fact]
-        public void BasicTest()
+        public void ShouldHandleTimeSmallerThanHalfAMinute()
         {
             var timeInMinutes = 0.4;
             var response = Utilities.HumanizeTime(timeInMinutes);
             response.Should().Be("less than a minute");
+        }
+        
+        [Fact]
+        public void ShouldHandleTimeJustOverHHalfAMinute()
+        {
+            var timeInMinutes = 0.68;
+            var response = Utilities.HumanizeTime(timeInMinutes);
+            response.Should().Be("1 minute");
+        }
+        
+        [Fact]
+        public void ShouldHandleTimeWellOverAMinute()
+        {
+            var timeInMinutes = 12.10;
+            var response = Utilities.HumanizeTime(timeInMinutes);
+            response.Should().Be("13 minutes");
         }
     }
 }
