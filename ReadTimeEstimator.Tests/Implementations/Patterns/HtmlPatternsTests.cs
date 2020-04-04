46f56d1ed855d6831792b9fbd116358c30b8aeb4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-04 18:28:13 +0100

    ft: add patterns tests

---
+++ ReadTimeEstimator.Tests/Implementations/Patterns/HtmlPatternsTests.cs
@@ -0,0 +1,29 @@
+using System;
+using FluentAssertions;
+using ReadTimeEstimator.Implementations.Patterns;
+using Xunit;
+
+namespace ReadTimeEstimator.Tests.Implementations.Patterns
+{
+    public class HtmlPatternsTests
+    {
+        [Fact]
+        public void ShouldInstantiatePatternSuccessfully()
+        {
+            Action action = () => new HtmlPatterns();
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ShouldInstantiatePatternWithExpectedValues()
+        {
+            var pattern = new HtmlPatterns();
+            pattern.Should().NotBeNull();
+            pattern.ImagePattern.Should().Be("<({img|Image})([\\w\\W]+?)[\\/]?>");
+            pattern.TagsPattern.Should().Be("<\\w+(\\s+(\"[^\"]*\"|\\\'[^\\\']*\'|[^>])+)?>|<\\/\\w+>");
+            pattern.WordsPattern.Should().Be("\\w+");
+            pattern.EastAsianCharSetPattern.Should()
+                .Be("[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]");
+        }
+    }
+}
---
+++ ReadTimeEstimator.Tests/Implementations/Patterns/MarkdownPatternsTests.cs
@@ -0,0 +1,29 @@
+using System;
+using FluentAssertions;
+using ReadTimeEstimator.Implementations.Patterns;
+using Xunit;
+
+namespace ReadTimeEstimator.Tests.Implementations.Patterns
+{
+    public class MarkdownPatternsTests
+    {
+        [Fact]
+        public void ShouldInstantiatePatternSuccessfully()
+        {
+            Action action = () => new MarkdownPatterns();
+            action.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ShouldInstantiatePatternWithExpectedValues()
+        {
+            var pattern = new MarkdownPatterns();
+            pattern.Should().NotBeNull();
+            pattern.ImagePattern.Should().BeNull();
+            pattern.TagsPattern.Should().BeNull();
+            pattern.WordsPattern.Should().Be("\\w+");
+            pattern.EastAsianCharSetPattern.Should()
+                .Be("[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]");
+        }
+    }
+}
