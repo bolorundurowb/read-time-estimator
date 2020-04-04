0570dcb4a28fd62a16c565366226608d228ff938
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-04 12:55:56 +0100

    ft: add in constants

---
+++ ReadTimeEstimator/Constants.cs
@@ -0,0 +1,11 @@
+namespace ReadTimeEstimator
+{
+    internal static class Constants
+    {
+        public const int WordsPerMinute = 275;
+
+        public const int ImageReadTimeInSeconds = 12;
+
+        public const int EastAsianCharactersPerMinute = 500;
+    }
+}
